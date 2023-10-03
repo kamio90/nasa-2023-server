fbd60731412f0d45b56c87c2fa1b1418a188d7b2
kamio90 <kmusial40@gmail.com>
2023-10-03 20:32:07 +0200

    chore: add mongobd in memory configuration

---
+++ jest-mongodb-config.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+module.exports = {
+  mongodbMemoryServerOptions: {
+    binary: {
+      version: '6.1.0',
+      skipMDS: true,
+    },
+    autoStart: false,
+    instance: {},
+  },
+}
