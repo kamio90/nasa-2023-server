3d120ab8d551f4b684d140e6dc173b5ad5c460df
kamio90 <kmusial40@gmail.com>
2023-10-07 16:13:04 +0200

    fix: reverse commit remove routers

--- src/application/routes/user.router.ts
+++ src/application/routes/user.router.ts
@@ -16,6 +16,86 @@ router.post('/register', async (req: Request, res: Response) => {
   }
 });
 
+router.get('/config/academic-title', async (req: Request, res: Response) => {
+  try {
+    const userService = new UserService();
+
+    const result = await userService.getUserAcademicTitle();
+
+    if (result.success) {
+      res.status(200).json({ message: result.message });
+    } else {
+      res.status(500).json({ message: 'Server error' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+router.get('/config/city', async (req: Request, res: Response) => {
+  try {
+    const userService = new UserService();
+
+    const result = await userService.getUserCity();
+
+    if (result.success) {
+      res.status(200).json({ message: result.message });
+    } else {
+      res.status(500).json({ message: 'Server error' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+router.get('/config/country', async (req: Request, res: Response) => {
+  try {
+    const userService = new UserService();
+
+    const result = await userService.getUserCountry();
+
+    if (result.success) {
+      res.status(200).json({ message: result.message });
+    } else {
+      res.status(500).json({ message: 'Server error' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+router.get('/config/education', async (req: Request, res: Response) => {
+  try {
+    const userService = new UserService();
+
+    const result = await userService.getUserEducation();
+
+    if (result.success) {
+      res.status(200).json({ message: result.message });
+    } else {
+      res.status(500).json({ message: 'Server error' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+router.get('/config/languages', async (req: Request, res: Response) => {
+  try {
+    const userService = new UserService();
+
+    const result = await userService.getUserLanguages();
+
+    if (result.success) {
+      res.status(200).json({ message: result.message });
+    } else {
+      res.status(500).json({ message: 'Server error' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
 router.post('/login', async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
